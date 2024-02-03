08cb8d53f8809c312424f5fcd88f9469e3114564
piyush visaria <piyush_v_n@piyushs-Mac-mini.local>
2024-02-03 17:21:14 +0530

    Tabs Assignment The Tabs Assignment is complete.
    The assignemnt is based Ionic framework
    It has Ionic angular components
    Addes gitignore

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SwiperModule } from 'swiper/angular';
+@NgModule({
+  declarations: [AppComponent],
+
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SwiperModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
